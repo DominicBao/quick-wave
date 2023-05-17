0229234cc1a9d09e8b9e2564e0e8b2ef8a7b82ed
Dominic <baozhifang@come-future.com>
2023-05-18 00:28:39 +0800

    feat: 修复warning

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import './common/common.less'
 
 import App from './page/app'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />)
